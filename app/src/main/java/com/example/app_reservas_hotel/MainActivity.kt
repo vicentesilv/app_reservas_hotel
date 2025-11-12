169780b75fbd2dad3c76278bd4e2613f73624e9b
Mario <astorgamhdez@gmail.com>
2025-11-12 08:34:02 -0700

    Implementacion de los cuadros de texto de login

--- app/src/main/java/com/example/app_reservas_hotel/MainActivity.kt
+++ app/src/main/java/com/example/app_reservas_hotel/MainActivity.kt
@@ -11,27 +11,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val output = findViewById<TextView>(R.id.textView)
-
-        Thread {
-            try {
-                val dbHelper = DatabaseHelper(this)
-
-                // Usar la sobrecarga que abre/cierra la BD internamente
-                val resultText = dbHelper.mostrarDatosPrueba()
-
-                runOnUiThread {
-                    output.text = resultText
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "Error al leer la base de datos", e)
-                runOnUiThread {
-                    val msg = e.message ?: "Desconocido"
-                    output.text = getString(R.string.error_db_access, msg)
-                }
-            }
-        }.start()
+        setContentView(R.layout.activity_login)
     }
 }
